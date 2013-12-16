e0e8318ef9d45057965a41cf926b85277b5d71cb
Feng-Zihao <feng.zh.zq@gmail.com>
2013-12-16 11:38:40 +0800

    Merge migrate files into one file.

--- db/migrate/20131216025420_deco_init.rb
+++ db/migrate/20131216025420_deco_init.rb
@@ -1,13 +1,35 @@
+
 require 'bcrypt'
 
-class CreateUsers < ActiveRecord::Migration
+
+class DecoInit < ActiveRecord::Migration
+
   def change
     create_table :users do |t|
       t.string :name
       t.string :encrypted_password
       t.timestamps
     end
+    add_index :users, :name, :unique => true, :using => :hash
+
+    create_table :daily_reports do |t|
+      t.belongs_to :user
+      t.date :date
+      t.text :commitment
+      t.text :plan
+      t.text :problem
+      t.timestamps
+    end
+
     User.create(name: "liu", encrypted_password: BCrypt::Password.create("liu"));
     User.create(name: "feng", encrypted_password: BCrypt::Password.create("feng"));
   end
+
+
+  def up
+  end
+
+  def down
+  end
 end
+
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20131213074940) do
+ActiveRecord::Schema.define(version: 20131216025420) do
 
   create_table "daily_reports", force: true do |t|
     t.integer  "user_id"
@@ -28,5 +28,7 @@ ActiveRecord::Schema.define(version: 20131213074940) do
     t.datetime "created_at"
     t.datetime "updated_at"
   end
+
+  add_index "users", ["name"], name: "index_users_on_name", unique: true
 
 end
