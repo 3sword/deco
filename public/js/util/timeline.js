82292edfd5b20b313e9a5851730a4c4d70e58cad
Feng-Zihao <feng.zh.zq@gmail.com>
2013-12-17 14:37:00 +0800

    Finish timeline template.

--- public/js/util/timeline.js
+++ public/js/util/timeline.js
@@ -1,28 +1,31 @@
 function timelineCtrl($scope) {
 
+    var DENSITY_MIN = 10;
+    var DENSITY_MAX = 20;
+
+
     // defaults one day before
     $scope.timeFrom = Date.now() - 1 * 1000 * 60 * 60 * 24;
     $scope.timeTo = Date.now();
-    $scope.density = 10;  // how many time points on the time line
+    $scope.density = 20;  // how many time duration on the time line
+                         // density + 1 points
     $scope.timestamps = [];  // in pair, time, left
 
+
     $scope.refresh = function() {
-        var step = ($scope.timeFrom - $scope.timeTo) / $scope.density;
-        var t = $scope.timeFrom;
+        var timeStep = ($scope.timeFrom - $scope.timeTo) / $scope.density;
+        var time = $scope.timeFrom;
         $scope.timestamps = [];
-        var divs = document.querySelectorAll("#top-timeline > div");
-        console.log(divs);
-        var totalWidth = divs[2].offsetLeft - divs[0].offsetRight;
-        console.log(divs[2].offsetLeft);
-        console.log(divs[0].offsetRight);
-        console.log(totalWidth);
-        //for (var i = 0; i <= $scope.density; i++) {
-        //    var o = {};
-        //    o['left'] = i * 10;
-        //    o['time'] = t;
-        //    $scope.timestamps.push(o);
-        //    t += step;
-        //}
+        var dateWidth = document.querySelector("#top-timeline > .date").offsetWidth;
+        var timestampsWidth = document.querySelector("#top-timeline").offsetWidth;
+        var widthStep = (timestampsWidth - dateWidth * 2) / $scope.density;
+        for (var i = 0; i <= $scope.density; i++) {
+            var o = {};
+            o['left'] = dateWidth + i * widthStep;
+            o['time'] = time;
+            $scope.timestamps.push(o);
+            time += timeStep;
+        }
     };
 
 };
