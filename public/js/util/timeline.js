8ec2e5dcf853e637ce1e0b9ea81cca80584790da
Feng-Zihao <feng.zh.zq@gmail.com>
2013-12-17 17:58:57 +0800

    Add timeline-operation panel. Can stick to mouse on timeline.

--- public/js/util/timeline.js
+++ public/js/util/timeline.js
@@ -1,25 +1,26 @@
-function timelineCtrl($scope) {
+function timelineCtrl($scope, $timeout) {
 
     var DENSITY_MIN = 10;
     var DENSITY_MAX = 20;
-
+    var timeSize = 20;
 
     // defaults one day before
     $scope.timeFrom = Date.now() - 1 * 1000 * 60 * 60 * 24;
     $scope.timeTo = Date.now();
-    $scope.density = 20;  // how many time duration on the time line
-                         // density + 1 points
     $scope.timestamps = [];  // in pair, time, left
+    $scope.mouseX = 0;
+    $scope.showOperation = true;
 
 
     $scope.refresh = function() {
-        var timeStep = ($scope.timeFrom - $scope.timeTo) / $scope.density;
+        var timeStep = ($scope.timeFrom - $scope.timeTo) / timeSize;
         var time = $scope.timeFrom;
         $scope.timestamps = [];
         var dateWidth = document.querySelector("#top-timeline > .date").offsetWidth;
         var timestampsWidth = document.querySelector("#top-timeline").offsetWidth;
-        var widthStep = (timestampsWidth - dateWidth * 2) / $scope.density;
-        for (var i = 0; i <= $scope.density; i++) {
+        // 2 date label and the last time label, which is 3 em in css.
+        var widthStep = (timestampsWidth - dateWidth * 2 - dateWidth * 3/4) / (timeSize-1);
+        for (var i = 0; i < timeSize; i++) {
             var o = {};
             o['left'] = dateWidth + i * widthStep;
             o['time'] = time;
@@ -27,6 +28,27 @@ function timelineCtrl($scope) {
             time += timeStep;
         }
     };
+
+    $scope.triggerTimeline = function(ev) {
+        $scope.showOperation = true;
+        console.log(ev.toElement.class);
+        if (ev.toElement.id == "top-timeline" || ev.toElement.classList.contains('time')) {
+            $scope.mouseX = ev.clientX - 30;
+        }
+        console.log(ev);
+        var time = function(ev) {
+            console.log(ev.clientX);
+            return 1;
+        }(ev);
+    }
+
+    $scope.hideOperationPanel = function(ev) {
+        $timeout(function() {
+            if (ev.toElement.id != "op-cursor") {
+                $scope.showOperation = false;
+            }
+        }, 1000);
+    }
 
 };
 
