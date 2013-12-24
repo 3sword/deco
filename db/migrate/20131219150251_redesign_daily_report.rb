7f928831a8fa34e94b5a64448addfe8a83c5d477
Liu Yang <liu_yang@worksap.co.jp>
2013-12-24 11:34:06 +0800

    Fix datatype of content.

--- db/migrate/20131219150251_redesign_daily_report.rb
+++ db/migrate/20131219150251_redesign_daily_report.rb
@@ -2,7 +2,7 @@ class RedesignDailyReport < ActiveRecord::Migration
     def change
         change_table(:daily_reports) do |t|
             t.remove :commitment, :plan, :problem
-            t.string :content
+            t.text :content
         end
     end
 end
