da941a0dcd575efc71da1f3e9bde661928b34786
Liu Yang <liu_yang@worksap.co.jp>
2014-01-29 16:09:34 +0800

    Fix small problems.

--- db/migrate/20140129080238_create_user_groups.rb
+++ db/migrate/20140129080238_create_user_groups.rb
@@ -1,25 +1,19 @@
-
-require 'bcrypt'
-
 class CreateUserGroups < ActiveRecord::Migration
-
     def change
         create_table :user_groups do |t|
             t.string :name
             ## 0: tobe cleanup, 1: temporary disable, 2: normal
-            t.integer :status, :null => false, :default => 2  
+            t.integer :status, :null => false, :default => 2
             t.timestamps
         end
-        add_index :user_groups, :name, :unique => true, :using => :hash
+        add_index :user_groups, :name
 
         create_table :user_group_members do |t|
-            t.belongs_to :user_group, :null => false
-            t.belongs_to :user,       :null => false
+            t.belongs_to :user_group
+            t.belongs_to :user
             ## 0: tobe cleanup, 1: normal, 2: manager
             t.integer :role, :null => false, :default => 1
             t.timestamps
         end
     end
-
 end
-
--- models/user_group.rb
+++ models/user_group.rb
@@ -2,7 +2,7 @@
 class UserGroup < ActiveRecord::Base
 
     has_many :user_group_members
-    has_many :users, through: :user_group_association
+    has_many :users, through: :user_group_members
 
     validates_uniqueness_of :name
 
